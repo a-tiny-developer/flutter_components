cc056339339ec336b792e8f6ab3a557b69046d78
a-tiny-developer <atinyflame@gmail.com>
2021-12-27 18:18:03 -0600

    arguments to widgets

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -16,11 +16,21 @@ class CardScreen extends StatelessWidget {
         children: const [
           CustomCardType1(),
           SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(
+            imageURL:
+                "https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFuZHNjYXBlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+          ),
           SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(
+            imageURL:
+                "https://images.unsplash.com/photo-1470770841072-f978cf4d019e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bGFuZHNjYXBlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+          ),
           SizedBox(height: 10),
-          CustomCardType2(),
+          CustomCardType2(
+            name: 'A river in the forest',
+            imageURL:
+                "https://images.unsplash.com/photo-1511884642898-4c92249e20b6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bGFuZHNjYXBlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60",
+          ),
           SizedBox(height: 100),
         ],
       ),
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -3,7 +3,14 @@ import 'package:flutter/material.dart';
 import '../theme/theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({Key? key}) : super(key: key);
+  const CustomCardType2({
+    Key? key,
+    required this.imageURL,
+    this.name,
+  }) : super(key: key);
+
+  final String imageURL;
+  final String? name;
 
   @override
   Widget build(BuildContext context) {
@@ -16,20 +23,20 @@ class CustomCardType2 extends StatelessWidget {
       shadowColor: AppTheme.primary.withOpacity(0.5),
       child: Column(
         children: [
-          const FadeInImage(
-            image: NetworkImage(
-                'https://images.unsplash.com/photo-1494500764479-0c8f2919a3d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bGFuZHNjYXBlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=1000&q=60'),
-            placeholder: AssetImage('assets/images/jar-loading.gif'),
+          FadeInImage(
+            image: NetworkImage(imageURL),
+            placeholder: const AssetImage('assets/images/jar-loading.gif'),
             width: double.infinity,
             height: 230,
             fit: BoxFit.fitHeight,
-            fadeInDuration: Duration(milliseconds: 300),
+            fadeInDuration: const Duration(milliseconds: 300),
           ),
-          Container(
-            alignment: AlignmentDirectional.centerEnd,
-            padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
-            child: const Text('A beautiful background'),
-          )
+        if (name != null)
+            Container(
+              alignment: AlignmentDirectional.centerEnd,
+              padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
+              child: Text(name ?? 'No Title'),
+            )
         ],
       ),
     );
