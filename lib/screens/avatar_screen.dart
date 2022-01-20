fe6aef5373e604acf6aedd269a4f44c753f1683c
a-tiny-developer <atinyflame@gmail.com>
2022-01-20 17:50:27 -0600

    changed network image - avatar screen

--- lib/screens/avatar_screen.dart
+++ lib/screens/avatar_screen.dart
@@ -23,7 +23,8 @@ class AvatarScreen extends StatelessWidget {
         child: CircleAvatar(
           maxRadius: 110,
           backgroundImage: NetworkImage(
-              'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=1000&q=60'),
+            'https://avatars.githubusercontent.com/u/93559957?v=4',
+          ),
         ),
       ),
     );
